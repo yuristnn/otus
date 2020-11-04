9ac37862d648bd239a95d81c99184677930bbe69
yuri.sidorov <yuri.sidorov@megafon.ru>
2020-11-04 15:03:56 +0300

    Added types to homework3

--- src/homework3/easy1.ts
+++ src/homework3/easy1.ts
@@ -3,7 +3,7 @@
 // Нужно заменить FIXME на тип который вычисляется на освове OrderState
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+type FIXME = Array<'initial' | 'inWork' | 'fullfilled'>;
 
 const orderStates = [
     'initial',
